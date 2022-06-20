9d7de4129d56bc816bee08e7e1b529e847cec31f
Arsen <arsen.sheikhulislamov@protonmail.com>
2022-06-20 18:54:33 +0300

    Adjusting the Custom Hook Logic

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
@@ -24,12 +24,12 @@ const useHttp = (requestConfig, applyData) => {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
     isLoading,
     error,
-    sendRequest
+    sendRequest,
   };
 };
 
